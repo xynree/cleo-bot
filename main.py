280c3f8cc6046431b16ba36b74f576475ac52175
xynree <xynree@gmail.com>
2021-08-16 00:57:40 -0600

    feed, pet, inside/outside listeners

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ client = discord.Client()
 
 load_dotenv()
 
-meow_list = ["mrowW", "mow!", "mewww", "mrew!", "MROWWW", "brrr", "mohwr", "meeeOW", "mrrrrrr", "MeOW", "meeewwwee!"]
+meow_list = ["mrowW", "mow!", "mewww", "mrew!", "MROWWW", "brrr", "mohwr", "meeeOW", "mrrrrrr", "MeOW", "meeewwwee!", "meeeeOwoOo", "meow", "mow mow", "meo meo mew!"]
+
+outside = False
+fed = False
+pet_count = 0
 
 @client.event
 async def on_ready():
@@ -15,13 +19,64 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global fed
+    global outside
+    global pet_count
+    meow = random.choice(meow_list)
+    msg = message.content
+    send = message.channel.send
+
     if message.author == client.user:
         return
 
-    msg = message.content
-
-    if msg.startswith('$cleo'):
-        await message.channel.send(random.choice(meow_list))
+    if msg == '$cleooutside':
+        if outside:
+            await send('>>> Cleo is already outside!')
+        else:
+            outside = True
+            await send(' >>> You put Cleo outside!')
+    if msg == '$cleo':
+        if outside:
+            await send('>>> Cleo is outside! Type $findcleo to search!')
+        else:
+            await send(meow)
+    if msg == '$findcleo':
+        if not(outside):
+            await send(meow)
+        else:
+            randomint = random.randint(0,9)
+            if randomint > 5:
+                outside = False
+                await send('>>> You put Cleo back inside!')
+                await send(meow)
+            else:
+                await send('>>> You could not find Cleo. Try again later!')
+    if msg == '$pet':
+        if outside:
+            await send('>>> Cleo is outside! Type $findcleo to search!')
+        else:
+            pet_count = pet_count + 1
+            if pet_count < 4:
+                if pet_count == 1:
+                    await send('>>> Cleo headbutts you happily.')
+                    await send(meow)
+                if pet_count == 2:
+                    await send('>>> Cleo sticks her butt up in the air.')
+                    await send(meow)
+                if pet_count == 3:
+                     await send('>>> Cleo wiggles under your hand.')
+                     await send(meow)
+            else:
+                await send('>>> Cleo gives you a little nip! Try again later.')
+                pet_count = 0
+    if msg == '$feed':
+        if outside:
+            await send('>>> Cleo is outside! Type $findcleo to search!')
+        else: 
+            fed = True
+            await send('>>> Cleo has been fed! She slurps up her food happily.')
+            await send(meow)
+    
 
 TOKEN = os.getenv("TOKEN");
 client.run(TOKEN)
